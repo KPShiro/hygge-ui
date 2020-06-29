0a5fbf383bee9a4661c89c8c04b65fefe6188617
Kacper Popczyński <popkacper@gmail.com>
2020-06-29 18:11:34 +0200

    Remove console log from CompanyProfilePage

--- src/app/pages/company-profile/company-profile.component.ts
+++ src/app/pages/company-profile/company-profile.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { IInvitation } from '@features/company/interfaces/invitation.interface';
-// import { CompanyFacadeService } from '@features/company/services/company-facade/company-facade.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 
@@ -20,14 +19,11 @@ export class CompanyProfileComponent implements OnInit {
   public constructor(
     private readonly _fb: FormBuilder,
     private readonly _route: ActivatedRoute,
-    // private readonly _companyFacade: CompanyFacadeService,
   ) { }
 
   public ngOnInit(): void {
     this.employees = this._route.snapshot.data.employees;
     this.invitations = this._route.snapshot.data.invitations;
-
-    console.table(this.employees);
 
     this.invitationForm = this._fb.group({
       email: [null, [Validators.email, Validators.required]],
