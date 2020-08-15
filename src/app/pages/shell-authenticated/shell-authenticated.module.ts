a632c0ecf9f1eab9f5aa0a516da5d755ae80d88f
Kacper Popczyński <popkacper@gmail.com>
2020-08-15 18:47:25 +0200

    Update NavBar

--- src/app/pages/shell-authenticated/shell-authenticated.module.ts
+++ src/app/pages/shell-authenticated/shell-authenticated.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { ShellAuthenticatedComponent } from './container/shell-authenticated.component';
 import { ShellAuthenticatedRoutingModule } from './shell-authenticated-routing.module';
 import { MainNavbarComponent } from '@app/common/components/main-navbar/main-navbar.component';
+import { SharedModule } from '@features/shared/shared.module';
 
 
 @NgModule({
     imports: [
         ShellAuthenticatedRoutingModule,
         CommonModule,
+        SharedModule,
     ],
     exports: [],
     declarations: [
