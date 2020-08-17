2434a69ebb04db38b64638a445d3344fb37ba63b
Kacper Popczyński <popkacper@gmail.com>
2020-08-18 00:27:58 +0200

    Update InvitationCard

--- src/app/pages/company-profile/components/invitation-card/invitation-card.component.ts
+++ src/app/pages/company-profile/components/invitation-card/invitation-card.component.ts
@@ -1,5 +1,4 @@
-import { Component, ChangeDetectionStrategy, Input, Output, EventEmitter, ViewChild } from '@angular/core';
-import { NgbTooltip } from '@ng-bootstrap/ng-bootstrap';
+import { Component, ChangeDetectionStrategy, Input, Output, EventEmitter } from '@angular/core';
 import { IInvitation } from '@features/company/interfaces/invitation.interface';
 import { OrganizationInvitationStatus } from '@app/common/enum/organization-invitation-status.enum';
 
@@ -13,9 +12,6 @@ import { OrganizationInvitationStatus } from '@app/common/enum/organization-invi
 export class InvitationCardComponent {
   @Input() public data: IInvitation;
   @Output() public delete: EventEmitter<string> = new EventEmitter();
-  @ViewChild('getLinkBtn') public getLinkBtn: NgbTooltip;
-
-  public canCopyLink = true;
 
   public get wasSent(): boolean {
     return this.data.status === OrganizationInvitationStatus.SENT;
@@ -39,21 +35,13 @@ export class InvitationCardComponent {
   }
 
   public onCopyLinkClicked(): void {
-    this.canCopyLink = false;
-    this.getLinkBtn.open();
-
     document.addEventListener('copy', this.copyUrlToClipboard.bind(this));
     document.execCommand('copy');
     document.removeEventListener('copy', this.onCopyLinkClicked.bind(this));
-
-    setTimeout(() => {
-      this.canCopyLink = true;
-      this.getLinkBtn.close();
-    }, 500);
   }
 
   private copyUrlToClipboard(event: any): void {
-    event.clipboardData.setData('text/plain', 'http://localhost:4200/sign-up?invitationId=' + this.data._id);
+    event.clipboardData.setData('text/plain', `${location.origin}/sign-up?invitationId=${this.data._id}`);
     event.preventDefault();
   }
 }
